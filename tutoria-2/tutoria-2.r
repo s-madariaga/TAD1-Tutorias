e14d877120387988a79db4e8462fb9802681b711
sofia <samadariaga@uc.cl>
2023-07-16 12:53:51 -0400

    add tutoria 2

---
+++ tutoria-2/tutoria-2.r
@@ -0,0 +1,59 @@
+
+###################################################.
+## Tutoría 2: Exploración de Bases de Datos en R ##
+###################################################.
+
+# Encabezado
+
+# Paquetes
+
+
+# 1. Importación y exportación de data.frames -----------------------------
+
+##* Recalcar: importancia del directorio
+##* Importación en diferentes formatos
+##* Exportación en diferentes formatos
+##* Importación eficiente vs. Formato tbl
+
+
+# 2. Sobre las funciones --------------------------------------------------
+
+##* Declarar una función.
+
+# 3. Funciones para la exploración inicial --------------------------------
+
+##* head
+##* tail
+##* dim
+##* names
+##* str
+##! Repaso a la exploración de variables:
+
+caracter <- "Holanda, qué talca?"
+entero <- 1L
+numeric <- 0.5
+logico <- TRUE
+
+## Comprobamos la clase a un tipo de dato
+
+is.character(character)
+
+## Pero estas funciones también se pueden vectorizar:
+
+## Corrección de las variables: afortunadamente, los vectores suelen contener un mismo tipo de dato. Así, solo queda corregir el formato, en caso de que la base se haya importado en el formato incorrecto.
+
+## Ejemplo: ¿qué hacer cuando debo corregir el formato de mis variables?
+
+##* is.na
+##* summarise
+
+
+
+
+# 4. Estadísticos descriptivos --------------------------------------------
+
+
+
+
+
+
